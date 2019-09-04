2ee0350725fd845e7d8272e55514d79937e1217a
ColtDahl <dahlcolt@gmail.com>
2019-09-04 17:30:39 -0500

    Add regex for input1

--- regex/regex.sh
+++ regex/regex.sh
@@ -1,4 +1,4 @@
 ##input0
 sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' r0_input.txt > r0_output.txt
-
-
+##input1
+sed -En 's/\* I am \b(KK|Nic|Vincent). My favorite sandwich is \b(turkey|avacado|ham).$/1. \1\n2. \2\n/p' r1_input.txt > r1_output.txt
