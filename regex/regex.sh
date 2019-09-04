5fa8efb2a6c8c282dd72bcd0e2ef387f97d4425c
ColtDahl <dahlcolt@gmail.com>
2019-09-04 17:38:08 -0500

    Add regex for input2

--- regex/regex.sh
+++ regex/regex.sh
@@ -2,3 +2,6 @@
 sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' r0_input.txt > r0_output.txt
 ##input1
 sed -En 's/\* I am \b(KK|Nic|Vincent). My favorite sandwich is \b(turkey|avacado|ham).$/1. \1\n2. \2\n/p' r1_input.txt > r1_output.txt
+##input2
+sed -E 's/\* sandwich with \b(turkey.bacon.swiss.|ham.cheddar.|tunaSalad.) \b(for here|to go)/1. \1\n2. \2\n/' r2_input.txt > r2_output.txt
+
